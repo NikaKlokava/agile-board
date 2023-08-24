339201b4d2bea31b682d43c449365e8c94a8f6b6
NikaKlokava <veronika.kazakevich1997@gmail.com>
2023-08-24 13:57:15 +0300

    feat: add layout for sidebar

--- src/pages/board/components/Sidebar.tsx
+++ src/pages/board/components/Sidebar.tsx
@@ -1,5 +1,32 @@
-import cl from "./styles.module.css"
+import classes from "classnames"
+import cl from "./styles.module.css";
 
 export const Sidebar = () => {
-    return <div className={cl.sidebar}>Sidebar</div>;
-  };
+  return (
+    <div className={cl.sidebar_wrapper}>
+      <div className={cl.sidebar_title}>{"ALL BOARDS (3)"}</div>
+      <div className={cl.board_title_items}>
+        <div className={cl.title_item}>
+          <div className={cl.icon} />
+          <p className={cl.title}>Board name</p>
+        </div>
+        <div className={cl.title_item}>
+          <div className={cl.icon} />
+          <p className={cl.title}>Board name</p>
+        </div>
+        <div className={cl.title_item}>
+          <div className={cl.icon} />
+          <p className={cl.title}>Board name</p>
+        </div>
+        <div className={cl.title_item}>
+          <div className={cl.icon_new_board} />
+          <p className={classes(cl.title, cl.new_board)}>New board</p>
+        </div>
+      </div>
+      <div className={cl.hide_sidebar}>
+        <div className={cl.hide_icon} />
+        <div className={cl.hide_text}>Hide sidebar</div>
+      </div>
+    </div>
+  );
+};
--- src/shared/components/header/Header.tsx
+++ src/shared/components/header/Header.tsx
@@ -4,6 +4,10 @@ import cl from "./header.module.css";
 export const Header = () => {
   return (
     <div className={cl.header_wrapper}>
+      <div className={cl.app_title}>
+        <div className={cl.app_logo}></div>
+        <h1 className={cl.title}>AGILE-BOARD</h1>
+      </div>
       <p className={cl.board_name}>Board name</p>
       <div className={cl.options}>
         <Button text={"+ Add New Task"} />
