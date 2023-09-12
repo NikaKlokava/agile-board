76ca4c61675f8736196a63de7736c6348dffb6bf
NikaKlokava <veronika.kazakevich1997@gmail.com>
2023-09-12 15:55:48 +0300

    fix: fix grid app template

--- src/pages/board/components/Sidebar.tsx
+++ src/pages/board/components/Sidebar.tsx
@@ -7,6 +7,7 @@ import classes from "classnames";
 import cl from "./styles/sidebar.module.css";
 
 export const Sidebar = () => {
+  const [sidebarVisible, setSidebarVisible] = useState<boolean>(true);
   const [newBoardVisible, setNewBoardVisible] = useState<boolean>(false);
 
   const boards = useSelector<RootState, Boards>((state) => state.boards.boards);
@@ -21,40 +22,49 @@ export const Sidebar = () => {
   );
 
   return (
-    <div className={cl.sidebar_wrapper}>
-      <div className={cl.sidebar_title}>{`ALL BOARDS (${boards.length})`}</div>
-      <div className={cl.board_title_items}>
-        {boards.map((board, index) => {
-          return (
-            <div
-              className={classes(
-                cl.title_item,
-                isEqual(board, activeBoard) && cl.active_board
-              )}
-              key={index}
-              onClick={() => dispatch(selectBoard(board))}
-            >
-              <div className={cl.icon} />
-              <p className={cl.title}>{board.name}</p>
-            </div>
-          );
-        })}
-        <div
-          className={cl.title_item}
-          data-testid="new-board"
-          onClick={() => setNewBoardVisible(true)}
-        >
-          <div className={cl.icon_new_board} />
-          <p className={classes(cl.title, cl.new_board)}>New board</p>
+    <>
+      {sidebarVisible && (
+        <div className={cl.sidebar_wrapper}>
+          <div
+            className={cl.sidebar_title}
+          >{`ALL BOARDS (${boards.length})`}</div>
+          <div className={cl.board_title_items}>
+            {boards.map((board, index) => {
+              return (
+                <div
+                  className={classes(
+                    cl.title_item,
+                    isEqual(board, activeBoard) && cl.active_board
+                  )}
+                  key={index}
+                  onClick={() => dispatch(selectBoard(board))}
+                >
+                  <div className={cl.icon} />
+                  <p className={cl.title}>{board.name}</p>
+                </div>
+              );
+            })}
+          </div>
+          <div
+            className={cl.title_item}
+            data-testid="new-board"
+            onClick={() => setNewBoardVisible(true)}
+          >
+            <div className={cl.icon_new_board} />
+            <p className={classes(cl.title, cl.new_board)}>New board</p>
+          </div>
+          {newBoardVisible && (
+            <NewBoardModal onClose={() => setNewBoardVisible(false)} />
+          )}
         </div>
-      </div>
-      <div className={cl.hide_sidebar}>
-        <div className={cl.hide_icon} />
-        <div className={cl.hide_text}>Hide sidebar</div>
-      </div>
-      {newBoardVisible && (
-        <NewBoardModal onClose={() => setNewBoardVisible(false)} />
       )}
-    </div>
+      <div
+        className={cl.hide_sidebar}
+        onClick={() => setSidebarVisible((prev) => !prev)}
+      >
+        <div className={sidebarVisible ? cl.hide_icon : cl.eye_icon} />
+        {sidebarVisible && <div className={cl.hide_text}>Hide sidebar</div>}
+      </div>
+    </>
   );
 };
